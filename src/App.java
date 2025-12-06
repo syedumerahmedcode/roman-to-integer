cfee934e1d1779df6f33545260c13489fa114708
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-12-06 20:14:21 +0100

    Feature:  Wrote problem statement as a comment in the main method.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /**
+         * Roman numerals are represented by 7 different symbols:
+         * I, V, X, L, C, D and M
+         * 
+         * Symbol --> Value
+         * I --> 1
+         * V --> 5
+         * X --> 10
+         * L --> 50
+         * C --> 100
+         * D --> 500
+         * M --> 1000
+         * 
+         * for example, 2 is written as II in Roman numeral,just ones added together.
+         * 12 is written as XII which is simply X + II.
+         * 27 is written as XVII which is X + V + II.
+         * 
+         * Roman numerals are usually written from largest to smallest, left to right.
+         * However, the numeral for four is not IIII, instead the number four is
+         * writtenas IV.
+         * Because the one is before the five, we subtract it making four.
+         * The same principle applies to number nine, which is written as IX.
+         * 
+         * There are six instances where subtraction is used:
+         * 
+         * I can be placed before V (5) and X (10) to make 4 and 9.
+         * X can be placed before L (50) and C (10) to make 40 and 90.
+         * C can be placed before D (500) and M (1000) to make 400 and 900.
+         * 
+         * Given a roman numeral, convert it to an integer.
+         * 
+         * 
+         * 
+         */
         System.out.println("Hello, World!");
     }
 }
